dee143235e2ac4641e569bb40ff3d327f62ac5f7
ZhaoGuibin <940508445@qq.com>
2019-01-26 10:49:37 +0800

    //这里似乎不需要cookie，先注释掉

--- zhihu.go
+++ zhihu.go
@@ -42,6 +42,7 @@ func getImgURL(url, imgDir string) {
 
 	//增加header选项
 	reqest.Header.Add("Content-Type", "application/json; charset=utf-8")
+	//这里似乎不需要cookie，先注释掉
 	//reqest.Header.Add("Cookie", "_zap=97a733fe-7d3e-45e0-9fe8-c138f01b6fc9; d_c0=\"AEBjiMYYoA2PTo-4jA6iymyc8bkORSrQyVg=|1526862212\"; _xsrf=rSY6NLBWiJVpd3t779cxSpkzOCTNZfnn; z_c0=Mi4xTURUVEFRQUFBQUFBUUdPSXhoaWdEUmNBQUFCaEFsVk4xakhKWEFBcmtpS09jQUFoVWpJM2NXNG9LQjBtbWxuWm5B|1541137366|ad106a49adcbac59f43323b0d4f97ded81c4a08b; __utmv=51854390.100-1|2=registration_date=20150706=1^3=entry_date=20150706=1; _ga=GA1.2.632016254.1541137374; tst=r; q_c1=c7a254833a0146769ea4f47f755774ba|1547085195000|1527140571000; __utmc=51854390; __utma=51854390.632016254.1541137374.1547085197.1547112488.3; __utmz=51854390.1547112488.3.3.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/people/zhao-gui-bin-zero/following/collections; tgw_l7_route=66cb16bc7f45da64562a077714739c11; arp_scroll_position=192745")
 	reqest.Header.Add("Host", "www.zhihu.com")
 	reqest.Header.Add("Referer", "www.zhihu.com")
